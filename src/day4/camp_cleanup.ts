f5f657ffc87ab26f57567e54a77638ee20e150e1
Daniele Megna <daniele.megna@casavo.com>
2022-12-26 12:45:53 +0100

    Refactoring in day 4 solution

--- src/day4/camp_cleanup.ts
+++ src/day4/camp_cleanup.ts
@@ -5,15 +5,24 @@ export const countOverlappingPairs = (input: string): number => {
 
     const parsedPairs = rows
         .map((row) => {
-            const matches = row.matchAll(/^(\d+)-(\d+),(\d+)-(\d+)$/g)
-            return [...matches][0].slice(1, 5).map(n => parseInt(n))
+            const matches = row.match(/^(\d+)-(\d+),(\d+)-(\d+)$/)!.slice(1)
+            const extractedNumbers = matches.map(n => parseInt(n))
+            return [
+                [extractedNumbers[0], extractedNumbers[1]] as Range,
+                [extractedNumbers[2], extractedNumbers[3]] as Range
+            ] as RangePair
         })
 
-    const overlappingPairs = parsedPairs
-        .filter(([firstStart, firstEnd, secondStart, secondEnd]) => {
-            return (firstStart >= secondStart && firstEnd <= secondEnd) ||
-                (secondStart >= firstStart && secondEnd <= firstEnd)
-        })
-
+    const overlappingPairs = parsedPairs.filter(areFullyOverlapping)
     return overlappingPairs.length
+}
+
+type Range = [number, number]
+type RangePair = [Range, Range]
+
+type OverlapRule = (ranges: RangePair) => boolean
+
+const areFullyOverlapping: OverlapRule = ([[firstStart, firstEnd], [secondStart, secondEnd]]) => {
+    return (firstStart >= secondStart && firstEnd <= secondEnd) ||
+        (secondStart >= firstStart && secondEnd <= firstEnd)
 }
