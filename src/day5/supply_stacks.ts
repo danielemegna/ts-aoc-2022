d9720830f3e88ab0bc7174c677bded234ea8d76a
Daniele Megna <daniele.megna@casavo.com>
2022-12-27 11:18:41 +0100

    Day 5: refactoring of parseStacksOfCrates

--- src/day5/supply_stacks.ts
+++ src/day5/supply_stacks.ts
@@ -4,34 +4,34 @@ export type RearrangementProcedure = any // TODO define this type
 
 export const parseInput = (input: string): [StacksOfCrates, RearrangementProcedure] => {
     const rows = input.split("\n")
-    
     const emptyRowIndex = rows.indexOf("")
-    const stacksOfCratesRows = rows.slice(0, emptyRowIndex-1)
-    const rearrangementProcedureRows = rows.slice(emptyRowIndex+1)
 
-    const stacksOfCrates = [] as StacksOfCrates
+    const stacksOfCratesRows = rows.slice(0, emptyRowIndex - 1)
+    const rearrangementProcedureRows = rows.slice(emptyRowIndex + 1)
+
+    return [parseStacksOfCrates(stacksOfCratesRows), null]
+}
+
+const parseStacksOfCrates = (stacksOfCratesRows: string[]): StacksOfCrates => {
+    const numberOfStacks = (stacksOfCratesRows[0].length + 1) / 4
+    const stacksOfCrates = newStacksOfCrates(numberOfStacks)
+
     stacksOfCratesRows.forEach((row) => {
-        var stackIndex = 0
-        var rowCursor = 1
-        while (true) {
-            const crateValue = row.slice(rowCursor, rowCursor + 1)
-
-            if (!stacksOfCrates[stackIndex])
-                stacksOfCrates[stackIndex] = []
+        for (var stackIndex = 0; stackIndex < numberOfStacks; stackIndex++) {
+            const crateValue = row[(stackIndex * 4) + 1]
 
             if (crateValue != " ")
                 stacksOfCrates[stackIndex].push(crateValue)
 
-            rowCursor += 4
-            stackIndex++
-            if (rowCursor >= row.length)
-                break
         }
 
     })
 
-    return [stacksOfCrates, null]
+    return stacksOfCrates
 }
+
+const newStacksOfCrates = (size: number): StacksOfCrates =>
+    [...new Array(size)].map(() => []) as StacksOfCrates
 
 export const finalTopCratesMessage = (input: string): string => {
     return "TILT" // TODO
