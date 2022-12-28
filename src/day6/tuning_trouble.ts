c1bee4161123df611f0d345532c72d6c0189f4d1
Daniele Megna <daniele.megna@casavo.com>
2022-12-28 09:15:38 +0100

    Day 6: clean and lint !

--- src/day6/tuning_trouble.ts
+++ src/day6/tuning_trouble.ts
@@ -1,24 +1,22 @@
 
 export const charsToFirstStartOfPacketMarker = (input: string): number => {
 
-    const acc = new Set<string>()
-    for(var i = 4; i<input.length; i++) {
-        const current = input.slice(i-4, i)
-        if(new Set(current).size == 4)
+    for (var i = 4; i < input.length; i++) {
+        const current = input.slice(i - 4, i)
+        if (new Set(current).size == 4)
             return i
     }
-    
+
     throw Error("Cannot find any start-of-packet marker")
 }
 
 export const charsToFirstStartOfMessageMarker = (input: string): number => {
 
-    const acc = new Set<string>()
-    for(var i = 14; i<input.length; i++) {
-        const current = input.slice(i-14, i)
-        if(new Set(current).size == 14)
+    for (var i = 14; i < input.length; i++) {
+        const current = input.slice(i - 14, i)
+        if (new Set(current).size == 14)
             return i
     }
-    
+
     throw Error("Cannot find any start-of-message marker")
 }
