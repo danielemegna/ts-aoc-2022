77250bfe065334ff5da3d4ac36676f498f8eb538
Daniele Megna <daniele.megna@casavo.com>
2023-01-27 08:46:50 +0100

    Day 8: slightly change provided input example
    ... to have a tree visible only from top

--- test/day8/treetop_tree_house.spec.ts
+++ test/day8/treetop_tree_house.spec.ts
@@ -4,7 +4,7 @@ import { treeMapFrom, visibleTreesCount } from '../../src/day8/treetop_tree_hous
 
 const providedInputExample =
     "30373\n" +
-    "25512\n" +
+    "65512\n" +
     "65332\n" +
     "33549\n" +
     "35390\n"
@@ -16,7 +16,7 @@ describe('first part resolution', () => {
             const treeMap = treeMapFrom(providedInputExample)
 
             expect(treeMap.getHeight([0, 0])).toBe(3)
-            expect(treeMap.getHeight([0, 1])).toBe(2)
+            expect(treeMap.getHeight([0, 1])).toBe(6)
             expect(treeMap.getHeight([1, 0])).toBe(0)
             expect(treeMap.getHeight([1, 1])).toBe(5)
             expect(treeMap.getHeight([2, 2])).toBe(3)
