297ff6166d7df3150f21825bb57f0db0a44f3481
Daniele Megna <daniele.megna@casavo.com>
2023-02-03 08:22:15 +0100

    Day 8: refactor TreeMap extracting private tree lines getter

--- src/day8/treetop_tree_house.ts
+++ src/day8/treetop_tree_house.ts
@@ -7,78 +7,6 @@ export class TreeMap {
         this.treeHeights = treeHeights
     }
 
-    getTreeHeight([x, y]: Coordinate): number {
-        return this.treeHeights[y][x]
-    }
-
-    getScenicScore([x, y]: Coordinate): number {
-        const mapSize: number = this.treeHeights.length
-        if (x === 0 || y === 0 || x === mapSize || y === mapSize)
-            return 0
-
-        const currentTree = this.treeHeights[y][x]
-
-        const treesOnTheTop = this.treeHeights.slice(0, y).map((row) => row[x]).reverse()
-        let topScore = 0
-        for(const x of treesOnTheTop) {
-            topScore++;
-            if(x >= currentTree)
-                break
-        }
-
-        const treesOnTheLeft = this.treeHeights[y].slice(0, x).reverse()
-        let leftScore = 0
-        for(const x of treesOnTheLeft) {
-            leftScore++;
-            if(x >= currentTree)
-                break
-        }
-
-        const treesOnTheBottom = this.treeHeights.slice(y + 1).map((row) => row[x])
-        let bottomScore = 0
-        for(const x of treesOnTheBottom) {
-            bottomScore++;
-            if(x >= currentTree)
-                break
-        }
-
-        const treesOnTheRight = this.treeHeights[y].slice(x + 1)
-        let rightScore = 0
-        for(const x of treesOnTheRight) {
-            rightScore++;
-            if(x >= currentTree)
-                break
-        }
-
-        return topScore * leftScore * bottomScore * rightScore
-    }
-
-    isVisible([x, y]: Coordinate): boolean {
-        const mapSize: number = this.treeHeights.length
-        if (x === 0 || y === 0 || x === mapSize || y === mapSize)
-            return true
-
-        const currentTree = this.treeHeights[y][x]
-
-        const treesOnTheTop = this.treeHeights.slice(0, y).map((row) => row[x]).reverse()
-        if (currentTree > Math.max(...treesOnTheTop))
-            return true
-
-        const treesOnTheLeft = this.treeHeights[y].slice(0, x).reverse()
-        if (currentTree > Math.max(...treesOnTheLeft))
-            return true
-
-        const treesOnTheBottom = this.treeHeights.slice(y + 1).map((row) => row[x])
-        if (currentTree > Math.max(...treesOnTheBottom))
-            return true
-
-        const treesOnTheRight = this.treeHeights[y].slice(x + 1)
-        if (currentTree > Math.max(...treesOnTheRight))
-            return true
-
-        return false
-    }
-
     getVisibleTreesCount(): number {
         let result = 0
         const mapSize = this.treeHeights.length
@@ -91,19 +19,108 @@ export class TreeMap {
         return result
     }
 
+    isVisible([x, y]: Coordinate): boolean {
+        const mapSize: number = this.treeHeights.length
+        if (x === 0 || y === 0 || x === mapSize || y === mapSize)
+            return true
+
+        const currentTree = this.treeHeights[y][x]
+
+        const treesOnTheTop = this.getTreesOnTheTop([x, y])
+        if (currentTree > Math.max(...treesOnTheTop))
+            return true
+
+        const treesOnTheLeft = this.getTreesOnTheLeft([x, y])
+        if (currentTree > Math.max(...treesOnTheLeft))
+            return true
+
+        const treesOnTheBottom = this.getTreesOnTheBottom([x, y])
+        if (currentTree > Math.max(...treesOnTheBottom))
+            return true
+
+        const treesOnTheRight = this.getTreesOnTheRight([x, y])
+        if (currentTree > Math.max(...treesOnTheRight))
+            return true
+
+        return false
+    }
+
     getHightestScenicScore(): number {
         let max = 0
         const mapSize = this.treeHeights.length
         for (let y = 0; y < mapSize; y++) {
             for (let x = 0; x < mapSize; x++) {
-                const score = this.getScenicScore([x,y])
+                const score = this.getScenicScore([x, y])
                 if (max < score)
                     max = score
             }
         }
         return max
-
     }
+
+    getScenicScore([x, y]: Coordinate): number {
+        const mapSize: number = this.treeHeights.length
+        if (x === 0 || y === 0 || x === mapSize || y === mapSize)
+            return 0
+
+        const currentTreeHeight = this.getTreeHeight([x, y])
+
+        const treesOnTheTop = this.getTreesOnTheTop([x, y])
+        let topScore = 0
+        for (const x of treesOnTheTop) {
+            topScore++;
+            if (x >= currentTreeHeight)
+                break
+        }
+
+        const treesOnTheLeft = this.getTreesOnTheLeft([x, y])
+        let leftScore = 0
+        for (const x of treesOnTheLeft) {
+            leftScore++;
+            if (x >= currentTreeHeight)
+                break
+        }
+
+        const treesOnTheBottom = this.getTreesOnTheBottom([x, y])
+        let bottomScore = 0
+        for (const x of treesOnTheBottom) {
+            bottomScore++;
+            if (x >= currentTreeHeight)
+                break
+        }
+
+        const treesOnTheRight = this.getTreesOnTheRight([x, y])
+        let rightScore = 0
+        for (const x of treesOnTheRight) {
+            rightScore++;
+            if (x >= currentTreeHeight)
+                break
+        }
+
+        return topScore * leftScore * bottomScore * rightScore
+    }
+    
+
+    getTreeHeight([x, y]: Coordinate): number {
+        return this.treeHeights[y][x]
+    }
+
+    private getTreesOnTheTop([x, y]: Coordinate): any {
+        return this.treeHeights.slice(0, y).map((row) => row[x]).reverse()
+    }
+
+    private getTreesOnTheLeft([x, y]: Coordinate): any {
+        return this.treeHeights[y].slice(0, x).reverse()
+    }
+
+    private getTreesOnTheBottom([x, y]: Coordinate): any {
+        return this.treeHeights.slice(y + 1).map((row) => row[x])
+    }
+
+    private getTreesOnTheRight([x, y]: Coordinate): any {
+        return this.treeHeights[y].slice(x + 1)
+    }
+
 }
 
 export function treeMapFrom(treeMapInput: string): TreeMap {
