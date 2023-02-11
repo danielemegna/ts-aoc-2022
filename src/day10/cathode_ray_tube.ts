1fd212ef180f46206a16e9ec706164e88f2aa21a
Daniele Megna <daniele.megna@casavo.com>
2023-02-11 11:10:09 +0100

    Day 10: start with program instruction parsing

---
+++ src/day10/cathode_ray_tube.ts
@@ -0,0 +1,25 @@
+export type Instruction = [InstructionType, number | null]
+export enum InstructionType { NOOP, ADDX }
+export type Program = Instruction[]
+
+export const parseInputInstructionFeed = (input: string): Program => {
+    return input
+        .split("\n")
+        .filter((row) => row !== "")
+        .map((row) => {
+            const [instruction, argument] = row.split(" ")
+            switch(instruction) {
+                case 'noop':
+                    return [InstructionType.NOOP, null] as Instruction
+                case 'addx':
+                    return [InstructionType.ADDX, parseInt(argument)] as Instruction
+            }
+
+            throw new Error(`Unexpected instruction ${row}`)
+        })
+}
+
+export const sumOfSixInterestingSignalStrengths = (input: string): number => {
+    const program: Program = parseInputInstructionFeed(input)
+    return -1
+}
